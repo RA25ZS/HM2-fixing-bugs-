11cf2e18eb7854850517939af2af2025114de08c
Andrey Ryshkevich <paulbegitson@mail.ru>
2022-07-21 21:25:39 +0200

    Checked and dogs are fixed

--- HW Fixing bugs/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ HW Fixing bugs/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class SpawnManagerX : MonoBehaviour
 {
-    public GameObject[] ballPrefabs;
+    public GameObject[] dogsPrefabs;
 
-    private float spawnLimitXLeft = -22;
-    private float spawnLimitXRight = 7;
-    private float spawnPosY = 30;
+    [SerializeField] float spawnLimitXLeft = -35.6f;
+    [SerializeField] float spawnLimitXRight = 7;
+    [SerializeField] float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    [SerializeField] float startDelay = 1.0f;
+    [SerializeField] float spawnInterval = 4.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +26,7 @@ public class SpawnManagerX : MonoBehaviour
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(dogsPrefabs[0], spawnPos, dogsPrefabs[0].transform.rotation);
     }
 
 }
